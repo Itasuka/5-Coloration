f0dccdb5e9655ab965b87fe8ca1674ee337de404
Itasuka <schivre.nicolas@gmail.com>
2023-12-28 23:36:32 +0100

    Documentation

--- projet.py
+++ projet.py
@@ -8,25 +8,38 @@ affichage = False
 verbose = False
 
 class Color(Enum):
+    """
+    Classe permettant de définir nos 5 couleurs et le fait qu'un sommet ne possède pas de couleur
+    """
     BLUE = "blue"
     RED = "red"
     GREEN = "green"
     WHITE = "white"
     BLACK = "black"
-    NO_COLOR = None
+    NO_COLOR = "yellow"
 
 class Vertex:
+    """
+    Classe représentant un sommet permettant de garder dans un seul et même endroit la couleur, les voisins et les coordonnées d'un sommet (les coordonnées peuvent ne pas exister)
+    """
     def __init__(self,color,voisins,coord=None):
         self.color = color
         self.voisins = voisins
         self.coord = None
 
-class Coordonnee:
+class Point:
+    """
+    Classe définissant un point avec ses 2 coordonnées x et y
+    """
     def __init__(self,x,y):
         self.x = x
         self.y = y
 
 def initGraph(file):
+    """
+    Effectue la lecture du fichier .graphe et créer le graphe sous forme de dictionnaire avec comme clé le numéro du sommet et la valeur un objet de classe Vertex\n
+    file = le chemin du fichier . graphe à lire
+    """
     graph = {}
     with open(file,'r') as graphFile:
         nbVertices = int(graphFile.readline())
@@ -41,16 +54,29 @@ def initGraph(file):
     return graph
 
 def brique1(graph):
+    """
+    Récupère un sommet de degré <= 5 s'il existe, renvoie None sinon\n
+    graph = Le graphe pour lequel on récupère le sommet
+    """
     for v in graph:
         if (len(graph[v].voisins) <= 5):
             return v
     return None
 
 def brique3(graph,v,colA,colB):
+    """
+    Echange les couleur colA et colB de la composante connexe ou se trouve le sommet v dans le graphe graph\n
+    graph = le graphe dans lequel on effectue le traitement\n
+    v = le numéro du sommet appartenant à la composante connexe à traiter\n
+    colA, colB = les 2 couleurs qui vont être échangées
+    """
     colorV = graph[v].color
     if colorV == colA or colorV == colB:
+        #On récupère le graphe contenant uniquement les sommets de couleurs colA et colB
         graphBicol = graphBicolor(graph,colA,colB)
+        #On parcours le graphe bicolore à partir du sommet v pour obtenir sa composante connexe
         compConnexe = BFS(graphBicol,v)
+        #On échange les couleurs
         for v in compConnexe:
             if compConnexe[v].color == colA:
                 compConnexe[v].color = colB
@@ -58,77 +84,125 @@ def brique3(graph,v,colA,colB):
                 compConnexe[v].color = colA
 
 def brique4_5(graph,v):
+    """
+    Renvoie une couleur non utilisée par les voisins de v dans graph (si elle existe)\n
+    graph = le graphe dans lequel on effectue le traitement\n
+    v = le sommet dont on cherche une couleur disponible
+    """
     possibility = [color for color in enumerate(Color) if color != Color.NO_COLOR]
     if (len(graph[v].voisins) <= 5):
+        #Pour chaque voisins on retire la possibilité de couleur correspondant à la couleur du voisin
         for neighbor in graph[v].voisins:
             if graph[neighbor].color in possibility:
                 possibility.remove(graph[neighbor].color)
+        #S'il y a une possibilité, on retourne la première
         if len(possibility) > 0:
             return possibility[0]
     return Color.NO_COLOR
 
-def brique6(graph,x):
+def brique6(graph,v):
+    """
+    Effectue le traitement de la brique 6 sur graph pour le sommet v\n
+    graph = le graphe dans lequel on effectue le traitement\n
+    v = le sommet dont on cherche la couleur
+    """
     possibility = [color for color in enumerate(Color) if color != Color.NO_COLOR]
-    for neighbor in graph[x].voisins:
+    for neighbor in graph[v].voisins:
         if graph[neighbor].color in possibility:
             possibility.remove(graph[neighbor].color)
+    #Si les voisins utilisent toutes les couleurs disponible
     if len(possibility) == 0:
+        #On créer un graphe sans v
         graphMinusX = graph.copy()
-        del graphMinusX[x]
+        graphMinusX = removeVertex(graphMinusX,v)
         compsConnexe = {}
-        for neighbor in graph[x].voisins:
+        #On récupère toutes les composantes connexes des voisins de x
+        for neighbor in graph[v].voisins:
             compsConnexe[neighbor] = BFS(graphMinusX,neighbor)
         for u in compsConnexe:
             compConnexe = compsConnexe[u]
-            for neighbor in graph[x].voisins:
+            #Pour chaque voisins de x
+            for neighbor in graph[v].voisins:
+                #S'il on trouve 2 sommets qui ne sont pas dans la même composante connexe
                 if not compConnexe[neighbor]:
                     colA = graph[u].color
                     colB = neighbor[u].color
+                    #On inverse, dans la composante connexe, les couleux correspondant au 2 sommets
                     brique3(compConnexe,u,colA,colB)
                     return colA
     return Color.NO_COLOR
 
 def coloring_rec(graph):
+    """
+    Effectue l'algorithme de coloration récursif sur graph\n
+    graph = le graphe à colorier
+    """
+    #Si le graphe n'est pas vide
     if len(graph.keys()) > 0:
         x = brique1(graph)
         graphMinusX = graph.copy()
         graphMinusX = removeVertex(graphMinusX,x)
+        #On fait un appel récursif sur G-x
         coloring_rec(graphMinusX)
+        #On applique la brique 4 et 5
         b4_5 = brique4_5(graph,x)
         if b4_5 == None:
+            #Si la brique4 et 5 ne renvoie pas de couleur on applique la brique 6
             b6 = brique6(graph,x)
             graph[x].color = b6
         else:
             graph[x].color = b4_5
 
 def brique7(graph):
-    sorted_list = sorted(graph, key=lambda x: len(graph[x].voisins), reverse=False)
+    """
+    Trie les sommets par nombre de voisins croissant afin que la brique 7 soit respectée
+    """
+    sorted_list = sorted(graph, key=lambda x: len(graph[x].voisins))
     return sorted_list
 
 def coloring_it(graph):
+    """
+    Effectue l'algorithme de coloration itératif sur graph\n
+    graph = le graphe à colorier
+    """
     list_order = brique7(graph)
+    #Pour i de (n-1 à 0)
     for i in range(len(graph)-1, -1, -1):
         v = list_order[i]
+        #On récupère les voisins de v qui ont un indice j supérieur à i dans liste_order
         neighbor = list(filter(lambda x: x in graph[v].voisins, list_order[i:]))
         graphDegenere = graph.copy()
+        #On retire les sommets qui ne font pas parti des n-i derniers éléments
         for j in range(i):
             graphDegenere = removeVertex(graphDegenere,list_order[j])
         graphDegenere[v].voisins = neighbor
+        #On applique la brique 4 et 5
         b4_5 = brique4_5(graphDegenere,v)
         if b4_5 == Color.NO_COLOR:
+            #Si la brique 4 et 5 ne renvoie pas de couleur on applique la brique 6
             b6 = brique6(graphDegenere,v)
             graph[v].color = b6
         else:
             graph[v].color = b4_5
 
 def checkColoring(graph):
+    """
+    Vérifie si un graphe est bien colorié\n
+    graph = le graphe à vérifier
+    """
     for v in graph:
         for neighbor in graph[v].voisins:
-            if graph[v].color == graph[neighbor].color:
+            #Chaque sommet doit avoir une couleur différente de ses voisins et une couleur différente de Color.NO_COLOR
+            if graph[v].color == graph[neighbor].color or graph[v].color[1] == Color.NO_COLOR:
                 return False
     return True
 
 def BFS(graph,v):
+    """
+    Effectue l'algorithme du Breadth-First Search (parcours en largeur)\n
+    graph = le graphe à parcourir\n
+    v = le point de départ du parcours
+    """
     compConnexe = {}
     f = [v]
     while f:
@@ -140,6 +214,11 @@ def BFS(graph,v):
     return compConnexe
 
 def graphBicolor(graph,colA,colB):
+    """
+    Récupère le sous-graphe de graph contenant uniquement les sommet de couleur colA ou colB\n
+    graph = le graphe d'origine\n
+    colA, colB = les couleurs pour lesquels on va garder les sommets
+    """
     res = {}
     for v in graph.keys():
         if graph[v].color == colA or graph[v].color == colB:
@@ -147,6 +226,11 @@ def graphBicolor(graph,colA,colB):
     return res
 
 def removeVertex(graph,v):
+    """
+    Retire un sommet de graph ainsi que toutes ses apparitions dans la liste des voisins d'un autre sommet\n
+    graph = graphe dans lequel on retire le sommet\n
+    v = le sommet à retirer
+    """
     if v in graph:
         for neighbor in graph[v].voisins:
             if v in graph[neighbor].voisins:
@@ -155,11 +239,19 @@ def removeVertex(graph,v):
     return graph
 
 def afficheGraphTerminal(graph):
+    """
+    Affiche la coloration de graph dans le terminal\n
+    graph = le graphe dont la coloration doit être affichée
+    """
     print(len(graph))
     for v in graph:
         print(v,": ",graph[v].color[1].value)
 
 def sauvegarderColoration(graph):
+    """
+    Sauvegarde dans un fichier .colors la coloration de graph\n
+    graph = le graph dont on veut sauvegarder la coloration
+    """
     res = ""
     for v in graph:
         res += str(v) + ": " + str(graph[v].color[1].value) + "\n"
@@ -167,6 +259,11 @@ def sauvegarderColoration(graph):
         fichier.write(res)
 
 def initCoord(graph,file):
+    """
+    Effectue la lecture du fichier .graphe et modifie graph pour définir l'attribut coord\n
+    graph = le graph correspondant au fichier . coords\n
+    file = le chemin du fichier . coords à lire
+    """
     with open(file,'r') as graphFile:
         nbVertices = int(graphFile.readline())
         for i in range(nbVertices):
@@ -175,11 +272,15 @@ def initCoord(graph,file):
             coord = [int(x) for x in ligne[1].strip('(').strip(')').split(',')]
             x = coord[0]
             y = coord[1]
-            graph[i].coord = Coordonnee(x,y)
+            graph[i].coord = Point(x,y)
         graphFile.close()
     return graph
 
 def affichageGraphWindow(graph):
+    """
+    Dessine graph dans une fenêtre extérieur à l'aide de la librairie matplotlib. Les sommets non coloriés apparaissent de couleur Color.NO_COLOR\n
+    graph = le graphe à dessiner
+    """
     fig, ax = plt.subplots()
     for v in graph:
         x = graph[v].coord.x
